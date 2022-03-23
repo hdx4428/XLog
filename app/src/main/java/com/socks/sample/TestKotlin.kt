fb9c674d62e4a048568de9ee4ab49abaa0290679
hdx4428 <2840830311@qq.com>
2022-03-23 16:07:36 +0800

    001

---
+++ app/src/main/java/com/socks/sample/TestKotlin.kt
@@ -0,0 +1,20 @@
+package com.socks.sample
+
+import android.util.Log
+import com.socks.library.KLog
+
+/**
+ *@author: HuangDaoXin
+ *@date: 2022/03/22
+ *@Description:
+ */
+class TestKotlin {
+
+
+    companion object{
+        fun testKotlinJump(){
+            KLog.d(  "TestAbc   test3()  ----------- ")
+        }
+    }
+
+}
